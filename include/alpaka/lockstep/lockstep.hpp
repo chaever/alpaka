c0a563b69786b2d1c1310a717f0e317cd14eefd7
Christian Kaever <s5926508@tu-dresden.de>
2024-02-27 18:00:52 +0100

    change GetNumWorkers to use Tags instead(this avoids some #if's)

---
+++ include/alpaka/lockstep/lockstep.hpp
@@ -0,0 +1,29 @@
+/* Copyright 2022-2023 Rene Widera
+ *
+ * This file is part of PMacc.
+ *
+ * PMacc is free software: you can redistribute it and/or modify
+ * it under the terms of either the GNU General Public License or
+ * the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PMacc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License and the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * and the GNU Lesser General Public License along with PMacc.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "alpaka/lockstep/ForEach.hpp"
+#include "alpaka/lockstep/Idx.hpp"
+#include "alpaka/lockstep/Kernel.hpp"
+#include "alpaka/lockstep/Variable.hpp"
+#include "alpaka/lockstep/Worker.hpp"
+#include "alpaka/lockstep/WorkerCfg.hpp"
