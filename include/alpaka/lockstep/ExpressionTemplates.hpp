fa852aa7c5ff143bd6dce9a1a110b8ed43818680
Christian Kaever <s5926508@tu-dresden.de>
2024-03-07 19:24:45 +0100

    add WritableExpression, add Assignment

--- include/alpaka/lockstep/ExpressionTemplates.hpp
+++ include/alpaka/lockstep/ExpressionTemplates.hpp
@@ -15,20 +15,38 @@ namespace alpaka::lockstep
     struct Addition{
         //should also work for Simd-types that define their own operator+
         template<typename T_Left, typename T_Right>
-        static constexpr decltype(auto) SIMD_EVAL_F(T_Left left, T_Right right){
+        static constexpr decltype(auto) SIMD_EVAL_F(T_Left const& left, T_Right const& right){
             //uses T_Left::operator+(T_Right)
             return left+right;
         }
     };
 
+    struct Assignment{
+        //should also work for Simd-types that define their own operator+
+        template<typename T_Left, typename T_Right, std::enable_if_t<!std::is_same_v<T_Right, Pack_t>, int> = 0>
+        static constexpr decltype(auto) SIMD_EVAL_F(T_Left& left, T_Right const& right){
+            //uses T_Left::operator=(T_Right)
+            return left=right;
+        }
+        template<typename T_Left, typename T_Right, std::enable_if_t< std::is_same_v<T_Right, Pack_t>, int> = 0>
+        static constexpr decltype(auto) SIMD_EVAL_F(T_Left& left, T_Right const& right){
+
+            //uses T_Left::operator=(T_Right)
+            return right.copy_to(&left);
+        }
+    };
+
+    //const left operand
     template<typename T_Left, typename T_Right, typename T_Functor>
     class Xpr{
         T_Left const& m_leftOperand;
         T_Right const& m_rightOperand;
     public:
-        Xpr(T_Left const & left, T_Right const & right):m_leftOperand(left), m_rightOperand(right)
+        Xpr(T_Left const& left, T_Right const& right):m_leftOperand(left), m_rightOperand(right)
         {
         }
+
+        using ThisXpr_t = Xpr<T_Left, T_Right, T_Functor>;
 
         template<typename T_Idx>
         constexpr auto const getValueAtIndex(T_Idx const i) const
@@ -37,11 +55,46 @@ namespace alpaka::lockstep
         }
 
         template<typename T_Other>
-        constexpr auto operator+(T_Other const & other){
-            using ThisXpr_t = Xpr<T_Left, T_Right, T_Functor>;
+        constexpr auto operator+(T_Other const & other) const
+        {
             return Xpr<ThisXpr_t, T_Other, Addition>(*this, other);
         }
 
-        
+        //writing to a readOnly expression is forbidden
+        template<typename T_Other>
+        constexpr auto operator=(T_Other const & other) = delete;
+    };
+
+    //non-const left operand to write to
+    template<typename T_Left, typename T_Right, typename T_Functor>
+    class WriteableXpr{
+        T_Left & m_leftOperand;
+        T_Right const& m_rightOperand;
+    public:
+        WriteableXpr(T_Left & left, T_Right const& right):m_leftOperand(left), m_rightOperand(right)
+        {
+        }
+
+        using ThisXpr_t = WriteableXpr<T_Left, T_Right, T_Functor>;
+
+        template<typename T_Idx>
+        constexpr auto const getValueAtIndex(T_Idx const i) const
+        {
+            //operator[] returns reference, which is then assignable
+            //the cast transforms any SimdLookupIndices into ints
+            return Assignment::SIMD_EVAL_F(m_leftOperand[static_cast<uint32_t>(i)], m_rightOperand.getValueAtIndex(i));
+        }
+
+        template<typename T_Other>
+        constexpr auto operator+(T_Other const & other) const
+        {
+            return Xpr<ThisXpr_t, T_Other, Addition>(*this, other);
+        }
+
+        template<typename T_Other>
+        constexpr auto operator=(T_Other const & other) const
+        {
+            return WriteableXpr<ThisXpr_t, T_Other, Assignment>(*this, other);
+        }
     };
 } // namespace alpaka::lockstep
