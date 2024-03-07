60e0dcb05984420f954c31e1a2b30e6cbfcaf38f
Christian Kaever <s5926508@tu-dresden.de>
2024-03-07 22:27:09 +0100

    fix errors

--- include/alpaka/lockstep/ExpressionTemplates.hpp
+++ include/alpaka/lockstep/ExpressionTemplates.hpp
@@ -6,6 +6,14 @@
 
 namespace alpaka::lockstep
 {
+
+    //prior to C++ 20 a static operator() is not allowed
+#if __cplusplus < 202002L
+    #define SIMD_EVAL_F eval
+#else
+    #define SIMD_EVAL_F operator()
+#endif
+
     namespace detail
     {
         template<typename T_Idx>
@@ -29,30 +37,24 @@ namespace alpaka::lockstep
             }
         };
 
-        //false
-        template<typename T>
-        struct IsPack{
-            static constexpr auto value = false;
+        template<typename T_Left, typename T_Right>
+        struct AssignmentTrait{
+            //assign scalars
+            static constexpr decltype(auto) SIMD_EVAL_F(T_Left& left, T_Right const& right){
+                //uses T_Left::operator=(T_Right)
+                return left=right;
+            }
         };
 
-        //true for Packs
-        template<typename T_Elem>
-        struct IsPack<typename alpaka::lockstep::Pack_t<T_Elem> >{
-            static constexpr auto value = true;
+        template<typename T_Left>
+        struct AssignmentTrait<T_Left, Pack_t<T_Left>>{
+            //assign packs
+            static constexpr decltype(auto) SIMD_EVAL_F(T_Left& left, Pack_t<T_Left> const& right){
+                SimdInterface_t<T_Left>::storeUnaligned(right, &left);
+                return right;//return right to preserve semantics of operator=
+            }
         };
-
-        //shortcut alias
-        template<typename T_Pack>
-        static constexpr auto isPack_v = IsPack<T_Pack>::value;
-
     } // namespace detail
-
-    //prior to C++ 20 a static operator() is not allowed
-#if __cplusplus < 202002L
-    #define SIMD_EVAL_F eval
-#else
-    #define SIMD_EVAL_F operator()
-#endif
 
     struct Addition{
         //should also work for Simd-types that define their own operator+
@@ -64,25 +66,25 @@ namespace alpaka::lockstep
     };
 
     struct Assignment{
-        //assign scalars
-        template<typename T_Left, typename T_Right, std::enable_if_t<!detail::isPack_v<T_Right>, int> = 0>
+        template<typename T_Left, typename T_Right>
         static constexpr decltype(auto) SIMD_EVAL_F(T_Left& left, T_Right const& right){
-            //uses T_Left::operator=(T_Right)
-            return left=right;
-        }
-        //assign packs
-        template<typename T_Left, typename T_Right, std::enable_if_t< detail::isPack_v<T_Right>, int> = 0>
-        static constexpr decltype(auto) SIMD_EVAL_F(T_Left& left, T_Right const& right){
-            SimdInterface_t<T_Right>::storeUnaligned(right, &left);
-            return right;//return right to preserve semantics of operator=
+            return detail::AssignmentTrait<T_Left, T_Right>::SIMD_EVAL_F(left, right);
         }
     };
+
+    //forward declarations
+    template<typename T_Functor, typename T_Left, typename T_Right>
+    class ReadXpr;
+    template<typename T_Functor, typename T_Left, typename T_Right>
+    class WriteableXpr;
 
     //cannot be assigned to
     //can be made from pointers, or some container classes
     template<typename T>
     class ReadLeafXpr{
         T const& m_source;
+    public:
+        using ThisXpr_t = ReadLeafXpr<T>;
         ///TODO maybe this class should know the size of the array it points to?
 
         ReadLeafXpr(T const& source) : m_source(source)
@@ -91,7 +93,7 @@ namespace alpaka::lockstep
 
         //allows making an expression from CtxVariable
         template<typename T_Config>
-        ReadLeafXpr(lockstep::Variable<T, T_Config> const& v) : m_source(v[0])
+        ReadLeafXpr(lockstep::Variable<T, T_Config> const& v) : m_source(v[lockstep::Idx(0,0)])
         {
         }
 
@@ -101,6 +103,12 @@ namespace alpaka::lockstep
         {
             return detail::IndexOperatorLeafRead<T_Idx>::eval(idx, &m_source);
         }
+
+        template<typename T_Other>
+        constexpr auto operator+(T_Other const & other) const
+        {
+            return ReadXpr<Addition, ThisXpr_t, T_Other>(*this, other);
+        }
     };
 
     //can be assigned to
@@ -108,6 +116,8 @@ namespace alpaka::lockstep
     template<typename T>
     class WriteLeafXpr{
         T & m_dest;
+    public:
+        using ThisXpr_t = WriteLeafXpr<T>;
         ///TODO maybe this class should know the size of the array it points to?
 
         WriteLeafXpr(T & dest) : m_dest(dest)
@@ -116,16 +126,22 @@ namespace alpaka::lockstep
 
         //allows making an expression from CtxVariable
         template<typename T_Config>
-        WriteLeafXpr(lockstep::Variable<T, T_Config> & v) : m_dest(v[0])
+        WriteLeafXpr(lockstep::Variable<T, T_Config> & v) : m_dest(v[lockstep::Idx(0,0)])
         {
         }
 
         //returns ref to allow assignment
         template<typename T_Idx>
-        auto & operator[](T_Idx const idx) const
+        auto & operator[](T_Idx const idx)
         {
             //static_cast will turn SimdLookupIndex into a flattened value when required
             return (&m_dest)[static_cast<uint32_t>(idx)];
+        }
+
+        template<typename T_Other>
+        constexpr decltype(auto) operator=(T_Other const & other)
+        {
+            return WriteableXpr<Assignment, ThisXpr_t, T_Other>(*this, other);
         }
     };
 
@@ -176,12 +192,6 @@ namespace alpaka::lockstep
             //operator[] returns reference, which is then assignable
             //the cast transforms any SimdLookupIndices into ints
             return T_Functor::SIMD_EVAL_F(m_leftOperand.getValueAtIndex(i), m_rightOperand.getValueAtIndex(i));
-        }
-
-        template<typename T_Other>
-        constexpr auto operator+(T_Other const & other) const
-        {
-            return ReadXpr<Addition, ThisXpr_t, T_Other>(*this, other);
         }
 
         template<typename T_Other>
