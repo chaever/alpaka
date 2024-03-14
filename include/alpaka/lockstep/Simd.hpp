ecc3ae03e1a7b0f49fc148539dc86357b4cf7464
Christian Kaever <s5926508@tu-dresden.de>
2024-03-14 22:57:53 +0100

    switch Simd Backend selection to tag types

--- include/alpaka/lockstep/Simd.hpp
+++ include/alpaka/lockstep/Simd.hpp
@@ -6,9 +6,21 @@
 
 namespace alpaka::lockstep
 {
+    namespace simdBackendTags{
+
+        class ScalarSimdTag{};
+        class ArrayOf4Tag{};
+        class StdSimdTag{};
+        class StdSimdFourTimesTag{};
+
+    }
+
+    template<typename T_Elem, typename T_Simd>
+    struct SimdInterface;
+
     //General-case SIMD interface, corresponds to a simdWidth of 1
-    template<typename T_Elem, typename T_Simd = void>
-    struct SimdInterface{
+    template<typename T_Elem>
+    struct SimdInterface<T_Elem, simdBackendTags::ScalarSimdTag>{
         using Elem_t = T_Elem;
         using Pack_t = T_Elem;//special case laneCount=1 : elements are packs
 
@@ -27,9 +39,29 @@ namespace alpaka::lockstep
 
     //Specialization using std::experimental::simd
     template<typename T_Elem>
-    struct SimdInterface<T_Elem, std::experimental::simd<T_Elem>>{
+    struct SimdInterface<T_Elem, simdBackendTags::StdSimdTag>{
         using Elem_t = T_Elem;
         using Pack_t = std::experimental::simd<T_Elem, std::experimental::simd_abi::native<T_Elem>>;
+
+        inline static constexpr std::size_t laneCount = Pack_t::size();
+
+        static ALPAKA_FN_INLINE ALPAKA_FN_HOST_ACC auto loadUnaligned(Elem_t const * const mem) -> Pack_t
+        {
+            return Pack_t(mem, std::experimental::element_aligned);
+        }
+
+        static ALPAKA_FN_INLINE ALPAKA_FN_HOST_ACC void storeUnaligned(const Pack_t t, Elem_t * const mem)
+        {
+            t.copy_to(mem, std::experimental::element_aligned);
+        }
+    };
+
+    //std::experimental::simd, but 4 at a time
+    template<typename T_Elem>
+    struct SimdInterface<T_Elem, simdBackendTags::StdSimdFourTimesTag>{
+        using Elem_t = T_Elem;
+        static constexpr auto sizeOfNativePack = std::experimental::simd<T_Elem, std::experimental::simd_abi::native<T_Elem> >::size();
+        using Pack_t =std::experimental::simd<T_Elem, std::experimental::simd_abi::fixed_size<sizeOfNativePack*4> >;
 
         inline static constexpr std::size_t laneCount = Pack_t::size();
 
@@ -49,7 +81,7 @@ namespace alpaka::lockstep
 
     //Specialization using 4 values in an array
     template<typename T_Elem>
-    struct SimdInterface<T_Elem, StdArrayWith4Elems<T_Elem>>{
+    struct SimdInterface<T_Elem, simdBackendTags::ArrayOf4Tag>{
         using Elem_t = T_Elem;
         using Pack_t = StdArrayWith4Elems<T_Elem>;
 
@@ -81,34 +113,25 @@ namespace alpaka::lockstep
     };
 
     //provides Information about the pack framework the user selected via CMake
-    struct selectedSIMDInfo{
-#if 1 || defined COMPILE_OPTION_FROM_CMAKE_1
-        template<typename T>
-        using simdNonBool_t = std::experimental::simd<T>;
-        //provides as many booleans as simdNonBool_t<T> can hold Ts
-        ///TODO decide if needed, can emulate by using other types
-        //template<typename T>
-        //using simdBool_t = std::experimental::simd_mask<T>;
+    namespace simdBackendTags{
+#if defined COMPILE_OPTION_FROM_CMAKE_1
+        using SelectedSimdBackendTag = simdBackendTags::StdSimdTag;
 #elif defined COMPILE_OPTION_FROM_CMAKE_2
-        template<typename T>
-        using simdNonBool_t = StdArrayWith4Elems<T>;
-        //template<typename T>
-        //using simdBool_t = StdArrayWith4Elems<bool>;
+        using SelectedSimdBackendTag = simdBackendTags::ArrayOf4Tag;
+#elif 1 || defined COMPILE_OPTION_FROM_CMAKE_3
+        using SelectedSimdBackendTag = simdBackendTags::StdSimdFourTimesTag;
 #else
-        template<typename T>
-        using simdNonBool_t = SimdInterface<T>;
-        //template<typename T>
-        //using simdBool_t = SimdInterface<T>;
+        using SelectedSimdBackendTag = simdBackendTags::ScalarSimdTag;
 #endif
-    };
-
-    //lane count for any type T, using the selected SIMD backend
-    template<typename T_Type>
-    static constexpr size_t laneCount = SimdInterface<T_Type, selectedSIMDInfo::simdNonBool_t<T_Type>>::laneCount;
+    }
 
     //conforms to the SimdInterface class above
     template<typename T_Type>
-    using SimdInterface_t = SimdInterface<T_Type, selectedSIMDInfo::simdNonBool_t<T_Type>>;
+    using SimdInterface_t = SimdInterface<T_Type, simdBackendTags::SelectedSimdBackendTag>;
+
+    //lane count for any type T, using the selected SIMD backend
+    template<typename T_Type>
+    static constexpr size_t laneCount = SimdInterface_t<T_Type>::laneCount;
 
     template<typename T_Type>
     using Pack_t = typename SimdInterface_t<T_Type>::Pack_t;
