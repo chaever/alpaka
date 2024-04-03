f2221b1c99df3ab73702e4c9379a74c01580e141
Christian Kaever <s5926508@tu-dresden.de>
2024-04-03 12:15:15 +0200

    wip, compiles, need more casts (only scalarSimd has one currently)

--- include/alpaka/lockstep/ExpressionTemplates.hpp
+++ include/alpaka/lockstep/ExpressionTemplates.hpp
@@ -107,12 +107,20 @@ namespace alpaka::lockstep
         template<typename T>
         static constexpr uint32_t getXprDims_v = detail::GetXprDims<std::decay_t<T>>::value;
 
+
+
+
 //operations like +,-,*,/ that dont modify their operands
 #define BINARY_READONLY_OP(name, shortFunc)\
         struct name{\
-            template<typename T_Left, typename T_Right>\
+            template<typename T_Left, typename T_Right, typename T_Sfinae = void>\
             ALPAKA_FN_HOST_ACC ALPAKA_FN_INLINE static constexpr decltype(auto) SIMD_EVAL_F(T_Left const& left, T_Right const& right){\
                 return left shortFunc right;\
+            }\
+            template<typename T_Left, typename T_Right, std::enable_if_t< (laneCount_v<T_Left> == laneCount_v<T_Right> && !std::is_same_v<T_Left, bool> && !std::is_same_v<T_Right, bool>), int> = 0 >\
+            decltype(auto) SIMD_EVAL_F(Pack_t<T_Left> const& left, Pack_t<T_Right> const& right){\
+                using result_elem_t = decltype(std::declval<T_Left>() shortFunc std::declval<T_Right>());\
+                return SimdInterface_t<result_elem_t>::elementWiseCast(left) shortFunc SimdInterface_t<result_elem_t>::elementWiseCast(right);\
             }\
             template<typename T_Other, typename T_Foreach, std::enable_if_t<!isXpr_v<T_Other>, int> = 0>\
             ALPAKA_FN_HOST_ACC ALPAKA_FN_INLINE static constexpr decltype(auto) makeRightXprFromContainer(T_Other const& other, T_Foreach const& forEach){\
@@ -390,7 +398,7 @@ namespace alpaka::lockstep
         public:
             T_Foreach const& m_forEach;
 
-            static_assert(!std::is_same_v<bool, T_Elem>);
+            static_assert(!std::is_same_v<bool, T_Elem>, "ReadLeafXpr currently cannot handle booleans as masks would be required (of which we dont know the size)");
 
             ReadLeafXpr(T_Foreach const& forEach, T_Elem const& source) : m_source(source), m_forEach(forEach)
             {
